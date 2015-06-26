f736e510c064dc4399260015aa607d68a443216b
Daniel D'Souza <daniel.dsouza5@gmail.com>
2015-06-26 11:27:03 -0700

    fixed domain, cmakeslists

--- scripts/speech2text.py
+++ scripts/speech2text.py
@@ -21,14 +21,14 @@ if __name__ == '__main__':
     text = rospy.Publisher("~speech", String, queue_size=1)
     confidence = rospy.Publisher("~confidence", Float32, queue_size=1)
     rate = rospy.Rate(10)
-    device = rospy.get_param("speech2text/device", "alsa default")
-    silence = rospy.get_param("speec2text/silence", "0 1 00:00:01.0 8%")
-    sox = rospy.get_param("speech2text/sox_command", 'sox -b 32 -e floating-point -r 44100 '
+    device = rospy.get_param("~device", "alsa default")
+    silence = rospy.get_param("~silence", "0 1 00:00:01.0 8%")
+    sox = rospy.get_param("~sox_command", 'sox -b 32 -e floating-point -r 44100 '
                                                      '-t {device} {file} vad silence {silence}')
-    get = rospy.get_param("speech2text/google_command", "wget --post-file='{file}' "
+    get = rospy.get_param("~google_command", "wget --post-file='{file}' "
                                                         "--header='Content-Type: audio/x-flac; rate=44100;' -O - "
                                                         "'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key={key}'")
-    key = rospy.get_param("speech2text/key", "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw")
+    key = rospy.get_param("~key", "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw")
 
     while not rospy.is_shutdown():
         rec = tempfile.NamedTemporaryFile('w+b', suffix='.flac')
